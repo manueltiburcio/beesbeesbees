8247778607d3512c7b9e52d4cda4bec5148687eb
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-07-25 10:54:55 -0700

    Implemented ForagerBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
+// this new constructor inheris from Bee, but it wont change Bee properties/methods!
 var ForagerBee = function() {
+  // get properties
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(string) {
+  this.treasureChest.push(string);
+}
