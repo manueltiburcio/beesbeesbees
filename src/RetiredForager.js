4e6446674bc019a079226b58179ebe1fd0830f96
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-07-25 11:16:48 -0700

    Implemented RetiredForager.js subclass

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,25 @@
+// this constructor inheris from Forager bee
 var RetiredForagerBee = function() {
+
+  ForagerBee.call(this);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
+
 };
 
+// get the methods from ForagerBee
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.gamble = function(string) {
+  this.treasureChest.push(string);
+}
