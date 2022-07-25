2de1c59ffc18f32e270716acaa89f05ccdc901a4
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-07-25 10:55:30 -0700

    Implemented js file

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
+// Our new constructor inherits from Bee subclass, but its not changing Bee!
 var HoneyMakerBee = function() {
+  // inherit properties
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+// get methods
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
